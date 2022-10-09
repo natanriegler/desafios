ee30b9b259878a92ef28a5d7a35c87530bfa6595
natanriegler <natan.riegler.silva@gmail.com>
2022-10-09 20:01:14 -0300

    Exercício 075

---
+++ Desafios Mundo 3/desafio075.py
@@ -0,0 +1,12 @@
+n1 = n2 = n3 = n4 = com9 = 0
+n1 = int(input('Digite o n1: '))
+n2 = int(input('Digite o n2: '))
+n3 = int(input('Digite o n3: '))
+n4 = int(input('Digite o n4: '))
+lista =(n1, n2, n3, n4)
+
+print(f'Ao todo foram {lista.count(9)} números 9')
+print(f'O número 3 apareceu pela primeira vez na posição {lista.index(3)}')
+
+
+
